ce42ff1f10509c6ad5d54342d9f5b82f5eccc0f9
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-04-18 22:30:30 +0530

    Creating Backend SpringBoot Project

---
+++ Backend/src/main/java/com/learnzilla/backend/LearnzillaApplication.java
@@ -0,0 +1,13 @@
+package com.learnzilla.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnzillaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/learnzilla/backend/LearnzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnzilla.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
