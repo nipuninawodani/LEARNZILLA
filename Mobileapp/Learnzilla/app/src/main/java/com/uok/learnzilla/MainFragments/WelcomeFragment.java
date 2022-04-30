10e6b2cbc35ef925c6cfd5de5f7c5f2c82767d1c
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2022-05-01 01:39:08 +0530

    Login activity Layout  and Bindings Done

---
+++ Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/MainFragments/WelcomeFragment.java
@@ -0,0 +1,46 @@
+package com.uok.learnzilla.MainFragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.uok.learnzilla.R;
+import com.uok.learnzilla.databinding.FragmentWelcomeBinding;
+
+
+public class WelcomeFragment extends Fragment {
+    private FragmentWelcomeBinding binding;
+
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.LogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(WelcomeFragment.this)
+                        .navigate(R.id.action_welcomeFragment_to_LoginFragment);
+            }
+        });
+    }
+}
