a080adf05aaaefd17c1b2ccd62aeb9b4fd9f81cf
waruniLalendra <warunilalendra@gmail.com>
2022-05-04 14:05:54 +0530

    Save signup data to database

---
+++ Backend/src/main/java/com/learnzilla/backend/signup/SignupController.java
@@ -0,0 +1,33 @@
+package com.learnzilla.backend.signup;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignupController {
+
+    private StudentRepository studentRepository;
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    public SignupController(StudentRepository studentRepository,TeacherRepository teacherRepository ) {
+
+        this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
+    }
+
+    @PostMapping("/signup/teacher")
+    public void signupTeacher(@RequestBody Teacher teacherData){
+
+        teacherRepository.save(teacherData);
+    }
+
+
+    @PostMapping("/signup/student")
+    public void signupStudent(@RequestBody Student studentData){
+
+        studentRepository.save(studentData);
+    }
+}
