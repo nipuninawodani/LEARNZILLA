ccef12b1c2fae8b80b870fd401f78209e0001c6a
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2022-05-05 18:30:34 +0530

    Registration NavArgs and Thread Menagement (#4)
    * Login activity Layout  and Bindings Done
    
    * Register User Dialog
    
    Layout :100:
    Bindings :+1:  but need to Implement Different Registration Fragments
    
    * Register Frontend Thread management and NavArgs Added for Registration
    
    Created Threads For registration Function

--- Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/DialogFragments/RegisterDialogFragment.java
+++ Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/DialogFragments/RegisterDialogFragment.java
@@ -1,6 +1,8 @@
 package com.uok.learnzilla.DialogFragments;
 
 
+import android.app.AlertDialog;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,8 +29,8 @@ public class RegisterDialogFragment extends DialogFragment {
         super.onStart();
             if (getDialog() == null)
                 return;
-            int dialogWidth = 1000; // specify a value here
-            int dialogHeight = 1000; // specify a value here
+            int dialogWidth = 1000;
+            int dialogHeight = 1000;
 
             getDialog().getWindow().setLayout(dialogWidth, dialogHeight);
             getDialog().getWindow().setBackgroundDrawableResource(R.drawable.dialog_round_bg);
@@ -44,9 +46,34 @@ public class RegisterDialogFragment extends DialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.Student.setOnClickListener(view1 ->
-                NavHostFragment.findNavController(RegisterDialogFragment.this)
-                        .navigate(R.id.action_RegisterDialog_to_RegisterFragment));
+
+        RegisterDialogFragmentDirections.ActionRegister action = RegisterDialogFragmentDirections.actionRegister();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+
+        binding.Student.setOnClickListener(view1 -> {
+            builder.setTitle("Warning");
+            builder.setMessage("Do you need to Create Account for Student ?")
+                    .setCancelable(false)
+                    .setPositiveButton("yes", (dialogInterface, i) -> NavHostFragment.findNavController(RegisterDialogFragment.this)
+                            .navigate(action.setRegister(1)))
+                    .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel());
+            AlertDialog alert = builder.create();
+            alert.show();
+        });
+
+        binding.teacher.setOnClickListener(view12 -> {
+            builder.setTitle("Warning");
+            builder.setMessage("To Register as a teacher you need to Wait for Admin Approval, Do you need To continue?")
+                    .setCancelable(false)
+                    .setPositiveButton("yes", (dialogInterface, i) -> NavHostFragment.findNavController(RegisterDialogFragment.this)
+                            .navigate(action.setRegister(2)))
+                    .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel());
+            AlertDialog alert = builder.create();
+            alert.show();
+        });
+
+
         binding.closeButton.setOnClickListener(View1 ->
                 dismiss());
     }
--- Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/MainFragments/RegisterFragment.java
+++ Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/MainFragments/RegisterFragment.java
@@ -1,5 +1,6 @@
 package com.uok.learnzilla.MainFragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,9 +8,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.uok.learnzilla.R;
 import com.uok.learnzilla.databinding.FragmentRegisterBinding;
@@ -18,12 +22,11 @@ public class RegisterFragment extends Fragment {
     private FragmentRegisterBinding binding;
 
     public RegisterFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -32,6 +35,31 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        int RegisterArg = RegisterFragmentArgs.fromBundle(getArguments()).getRegister();
+        if (RegisterArg == 1){
+            binding.registerTitle.setText("Student Register");
+        }
+        if (RegisterArg == 2){
+            binding.registerTitle.setText("Teacher Register");
+        }
+
+        binding.SignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if (RegisterArg == 1){
+                   RegisterStudent();
+                   NavHostFragment.findNavController(RegisterFragment.this)
+                           .navigate(R.id.action_RegisterFragment_to_LoginFragment);
+               }
+               if (RegisterArg == 2){
+                   RegisterTeacher();
+                   NavHostFragment.findNavController(RegisterFragment.this)
+                           .navigate(R.id.action_RegisterFragment_to_LoginFragment);
+               }
+            }
+        });
+
         binding.login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,5 +67,53 @@ public class RegisterFragment extends Fragment {
                         .navigate(R.id.action_RegisterFragment_to_LoginFragment);
             }
         });
+    }
+
+    //add Register Teacher Program
+    private void RegisterTeacher() {
+        ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading...");
+        progressDialog.setTitle("Register Teacher");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                   //Add Register
+                    Looper.prepare();
+                    Toast.makeText(getContext(), "Registration Completed", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                progressDialog.dismiss();
+            }
+        }).start();
+
+    }
+
+    //add Register Student Program
+    private void RegisterStudent() {
+        ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading...");
+        progressDialog.setTitle("Register Student");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    //Add Register
+                    Looper.prepare();
+                    Toast.makeText(getContext(), "Registration Completed", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                progressDialog.dismiss();
+            }
+        }).start();
+
     }
 }
