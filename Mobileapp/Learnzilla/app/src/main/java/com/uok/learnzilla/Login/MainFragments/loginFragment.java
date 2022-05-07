8b781959ed7c1675cfdcde2e260fd7843317aaaf
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2022-05-07 23:27:40 +0530

    Input validators Added
    Email pattern validated
    Confirm password Validated
    error messages added
    data binding fixed

--- Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/Login/MainFragments/loginFragment.java
+++ Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/Login/MainFragments/loginFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +18,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.uok.learnzilla.BackEndClasses.EmailPatternValidator;
 import com.uok.learnzilla.Home.HomeActivity;
 import com.uok.learnzilla.R;
 import com.uok.learnzilla.databinding.FragmentLoginBinding;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class loginFragment extends Fragment {
@@ -45,19 +50,44 @@ public class loginFragment extends Fragment {
                 .navigate(R.id.action_LoginFragment_to_RegisterDialog));
 
         binding.cirLoginButton.setOnClickListener(view12 -> {
-            LoginMethod();
-            goToHomeActivity();
+            binding.textInputEmail.setError(null);
+            binding.textInputPassword.setError(null);
+
+            //check is Email EditText Empty
+            if(TextUtils.isEmpty(binding.editTextEmail.getText().toString())){
+                binding.textInputEmail.setError("Error:Email Can't be empty");
+                return;
+            }
+
+            //Validate Email Patten
+            EmailPatternValidator EValidator = new EmailPatternValidator();
+            boolean validMail = EValidator.isEmailValid(binding.editTextEmail.getText().toString());
+            if(!validMail){
+                binding.textInputEmail.setError("Error:Wrong Mail Format, please check");
+                return;
+            }
+
+
+            //check is Password EditText is Empty
+            if(TextUtils.isEmpty(binding.editTextPassword.getText().toString())){
+                binding.textInputPassword.setError("Error:It's an Empty Password");
+                return;
+            }
+
+            //all pass F try to login
+                LoginMethod();
+
         });
     }
 
     private void goToHomeActivity() {
         Intent intent = new Intent(getActivity(), HomeActivity.class);
         startActivity(intent);
-
-
     }
 
 
+
+    //login
     private void LoginMethod() {
         ProgressDialog progressDialog = new ProgressDialog(getContext());
         progressDialog.setMessage("Loading...");
@@ -68,18 +98,20 @@ public class loginFragment extends Fragment {
 
         new Thread(() -> {
             try {
+                //Add Login
                 //save login
                 SharedPreferences sharedPreferences = getContext().getSharedPreferences("login",MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putBoolean("Login",true);
                 editor.apply();
-                //Add Login
                 Looper.prepare();
                 Toast.makeText(getContext(), "Login Completed", Toast.LENGTH_SHORT).show();
+                goToHomeActivity();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             progressDialog.dismiss();
         }).start();
     }
+
 }
