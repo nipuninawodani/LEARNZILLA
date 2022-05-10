2f1b8ed43e29a95f5194407733dfa75cea92794a
waruniLalendra <warunilalendra@gmail.com>
2022-05-10 20:48:09 +0530

    Add getter-setter pattern to student and teacher models

--- Backend/src/main/java/com/learnzilla/backend/signup/models/Teacher.java
+++ Backend/src/main/java/com/learnzilla/backend/signup/models/Teacher.java
@@ -10,9 +10,49 @@ public class Teacher {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    public int id;
-    public String firstName;
-    public String secondName;
-    public String Email;
-    public String password;
+    private int id;
+    private String firstName;
+    private String secondName;
+    private String Email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
