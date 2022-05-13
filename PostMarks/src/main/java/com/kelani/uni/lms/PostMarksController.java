bee3b26a55cad72ee02f85655e7e7116e52f35ba
nipuninawodani <nipuninawodani999@gmail.com>
2022-05-14 01:58:41 +0530

    Get and Post methos for PostMarks V1 is done

--- PostMarks/src/main/java/com/kelani/uni/lms/PostMarksController.java
+++ PostMarks/src/main/java/com/kelani/uni/lms/PostMarksController.java
@@ -1,13 +1,23 @@
 package com.kelani.uni.lms;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostMarksController {
 
-    @GetMapping("/postmarks")
+    @GetMapping("/marks")
     public String postMarks(){
-        return "SE/2018/001,A";
+        return "SE/2018/001,Abdullah M.R.M.,A";
+    }
+
+    @PostMapping("/marks")
+    public void addMarks(@RequestBody Student StudentMarks){
+        System.out.println("Student ID :"+ StudentMarks.id);
+        System.out.println("Student Name :"+ StudentMarks.name);
+        System.out.println("Student Grade :"+ StudentMarks.grade);
+        //place to save data in the server
     }
 }
---
+++ PostMarks/src/main/java/com/kelani/uni/lms/Student.java
@@ -0,0 +1,7 @@
+package com.kelani.uni.lms;
+
+public class Student {
+    public String id;
+    public String name;
+    public String grade;
+}
