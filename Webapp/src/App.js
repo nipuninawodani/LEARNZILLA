86224c37438e89648f9bb946a9414a06571190aa
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-05-16 17:11:18 +0530

    login v2

--- Webapp/src/App.js
+++ Webapp/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Routes, Link, Route} from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login'
-import Register from './components/Register/Register'
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
--- Webapp/src/index.js
+++ Webapp/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
