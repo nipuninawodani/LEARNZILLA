f0078ba185993a35e8cbe9c8c3745ba8ead4fb5c
waruniLalendra <warunilalendra@gmail.com>
2022-05-17 13:45:12 +0530

    Register component UI created

--- Webapp/src/Components/Register/Register.jsx
+++ Webapp/src/Components/Register/Register.jsx
@@ -1,13 +1,74 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
+import logo from './head-logo.png'
+import registerImg from './body-img.png'
+import './Register.css'
 
 function Register() {
 
+const[email,setEmail] = useState('');
+const[Fusername,setFname] = useState('');
+const[Lusername,setLname] = useState('');
+const[pwd,setPwd] = useState('');
+const[cpwd,setCpwd] = useState('');
+
+const handleSubmit=() => {
+    //e.preventDefault();
+}
+
+
+
     return (
-        <div>
-            <Link to='/Login'>Login</Link>
-            <br />
-            Register Page
+        <div className= 'main-Register'>
+
+            <div className="left-side">
+                <div className="header">
+                    <img src={logo} id="logo-img" alt="" />
+                </div>
+
+                <div className="body">
+                    <img src={registerImg} id="body-img" alt="" />
+                </div>
+
+                <p> Welcome to LearnZilla! </p>
+            </div>
+
+            <div className="right-side">
+                <div className="top-right">
+                    <p>
+                    Already have an Account?
+                    </p>
+                    <Link id="link-signIn" to='/Login'>Sign In</Link>
+                </div>
+                <div className="body-right">
+                    <div className="container">
+                        <h1>Create Account!</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-group">
+                                <h5>First Name</h5>
+                                <input type="text" name="Fname" value={Fusername} onChange={ e=>{setFname(e.target.value)} } id="fname"/>
+                            </div>
+                            <div className="input-group">
+                                <h5>Last Name</h5>
+                                <input type="text" name="Lname" value={Lusername} onChange={ e=>{setLname(e.target.value)} } id="lname"/>
+                            </div>
+                            <div className="input-group">
+                                <h5>Email</h5>
+                                <input type="Email" name="Email" value={email} onChange={ e=>{setEmail(e.target.value)} } id="email"/>
+                            </div>
+                            <div className="input-group">
+                                <h5>Password</h5>
+                                <input type="password" name="Password" value={pwd} onChange={ e=>{setPwd(e.target.value)} } id="pwd"/>
+                            </div>
+                            <div className="input-group">
+                                <h5>Confirm Password</h5>
+                                <input type="password" name="Confirm_password" value={cpwd} onChange={ e=>{setCpwd(e.target.value)} } id="c_pwd"/>
+                            </div>
+                            <input type="submit" id="s_btn" value="Submit" />
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
