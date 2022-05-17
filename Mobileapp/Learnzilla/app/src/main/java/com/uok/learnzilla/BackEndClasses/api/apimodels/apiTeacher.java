c8bd898e670ac0bc4595e19b9d231a010ac23398
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2022-05-17 16:34:46 +0530

    MobileBackend Retrofit Api Services

---
+++ Mobileapp/Learnzilla/app/src/main/java/com/uok/learnzilla/BackEndClasses/api/apimodels/apiTeacher.java
@@ -0,0 +1,31 @@
+package com.uok.learnzilla.BackEndClasses.api.apimodels;
+
+public class apiTeacher {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public apiTeacher(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
