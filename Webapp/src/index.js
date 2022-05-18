b6ba80d4c7c2c102d46ea5a2d6934a162c4f3a6a
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-05-18 08:36:23 +0530

    fixed error in index.js

--- Webapp/src/index.js
+++ Webapp/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
