f135111769900b23daf544c8e8ca2e85589baf3f
waruniLalendra <warunilalendra@gmail.com>
2022-05-19 21:32:10 +0530

    Course_view component initiated

---
+++ Webapp/src/Components/Course_view/Course_view.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Course_view.css"
+
+
+function Course_view() {
+
+    return (
+        <div className="container-course_view">
+            <div className="wrap-course_view">
+                Course view
+            </div>
+        </div>
+    )
+}
+
+export default Course_view;
