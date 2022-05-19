dfd856b82d2c28160860f990555db52dd5ca7652
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-05-19 13:28:35 +0530

    Added course view, added enrollment

--- Webapp/src/Components/Enroll/Enroll.jsx
+++ Webapp/src/Components/Enroll/Enroll.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link, useSearchParams} from "react-router-dom";
-import cs01 from './img/course/cs-1.jpg'
+import cs01 from './img/cs-1.jpg'
 import './css/Enroll.css'
 import {faCaretRight, faEnvelope, faHeart} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -10,7 +10,7 @@ function Enroll() {
     const [searchParams] = useSearchParams();
 
 
-    let CourseCode = searchParams.get("CourseCode")
+    let enrollCode = searchParams.get("enrollCode")
     let AcademicYear = searchParams.get("AcademicYear")
 
     return (
@@ -19,22 +19,22 @@ function Enroll() {
                 <div class="col-md-1"></div>
 
                 <div class="col-md-10">
-                    <div id="course-details" className="course-details-section">
+                    <div id="enroll-details" className="enroll-details-section">
                         <div className="container">
                             <div className="row">
                                 <div className="col-md-9">
-                                    <div className="course-details-item">
-                                        <div className="course-single-pic mb30">
+                                    <div className="enroll-details-item">
+                                        <div className="enroll-single-pic mb30">
                                             <img src={cs01} alt="" />
                                         </div>
-                                        <div className="course-single-text">
-                                            <div className="course-title mt10 headline relative-position">
+                                        <div className="enroll-single-text">
+                                            <div className="enroll-title mt10 headline relative-position">
                                                 <h3><a
-                                                    href="file:///C:/Users/Mahela/Downloads/Compressed/geniuscoursehtml-10/geniuscoursehtml-10/genius-course-html-upload-package/Genius_course_placeholder/course-details.html#">Fully
+                                                    href="file:///C:/Users/Mahela/Downloads/Compressed/geniusenrollhtml-10/geniusenrollhtml-10/genius-enroll-html-upload-package/Genius_enroll_placeholder/enroll-details.html#">Fully
                                                     Responsive <b>Web Design &amp; Development.</b></a>
                                                 </h3>
                                             </div>
-                                            <div className="course-details-content">
+                                            <div className="enroll-details-content">
                                                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
                                                     euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad
                                                     minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
@@ -58,9 +58,9 @@ function Enroll() {
 
                                 <div className="col-md-3">
                                     <div className="side-bar">
-                                        <div className="course-side-bar-widget">
+                                        <div className="enroll-side-bar-widget">
                                             <div className="btn btn-danger text-center text-uppercase float-left bold-font box">
-                                                Enroll THis course <FontAwesomeIcon icon={faCaretRight} />
+                                                Enroll THis enroll <FontAwesomeIcon icon={faCaretRight} />
                                             </div>
                                         </div>
                                         <div className="enrolled-student">
