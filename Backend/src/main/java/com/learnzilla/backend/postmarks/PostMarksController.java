4cb5adebccd9420e4a9fc1198c9774023b9230c6
nipuninawodani <nipuninawodani999@gmail.com>
2022-05-19 16:21:03 +0530

    Post Marks Function Completed

--- Backend/src/main/java/com/learnzilla/backend/postmarks/PostMarksController.java
+++ Backend/src/main/java/com/learnzilla/backend/postmarks/PostMarksController.java
@@ -1,23 +1,51 @@
 package com.learnzilla.backend.postmarks;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.learnzilla.backend.models.Students;
+import com.learnzilla.backend.models.PostMarks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PostMarksController {
 
-    @GetMapping("/marks")
-    public String postMarks(){
-        return "SE/2018/001,Abdullah M.R.M.,A";
+    private  PostMarksRepository postMarksRepository;
+
+    @Autowired
+    public PostMarksController(PostMarksRepository postMarksRepository){
+        this.postMarksRepository= postMarksRepository;
     }
 
-    @PostMapping("/marks")
-    public void addMarks(@RequestBody Student StudentMarks){
-        System.out.println("Student ID :"+ StudentMarks.id);
-        System.out.println("Student Name :"+ StudentMarks.name);
-        System.out.println("Student Grade :"+ StudentMarks.grade);
-        //place to save data in the server
+    @GetMapping("/postMarks/student_id={student_id}")
+    public ResponseEntity<List<PostMarks>> getPostMarksById(@PathVariable String student_id){
+        List<PostMarks> postMarks = postMarksRepository.findByStudent_id(student_id);
+        return ResponseEntity.ok(PostMarks);
     }
+
+    @PostMapping("/postMarks")
+    public void postMarks(@RequestBody PostMarks postMarksData){
+        postMarksRepository.save(postMarksData);
+    }
+
+    @PostMapping("/postMarks/edit")
+    public void updatePostMarks(@RequestBody PostMarks postMarksData) {
+
+        PostMarks  postMarks = postMarksRepository.findByPostMarksid(postMarksData.getPostMarksid());
+        if (postMarksData.getStudent_id()!=null){
+            postMarks.setStudent_id(postMarksData.getStudent_id());
+        }
+
+        if (postMarksData.getMarks()!=null){
+            lectureResource.setMarks(postMarksData.getMarks());
+        }
+        postMarksRepository.save(postMarksData);
+    }
+
+    @PostMapping("/postMarks/delete")
+    public void deletePostMarks(@RequestBody PostMarks postMarksData) {
+        postMarksRepository.deleteAllByPostMarksid(postMarksData.getPostMarksid());
+    }
+
 }
--- Backend/src/main/java/com/learnzilla/backend/postmarks/Student.java
+++
@@ -1,7 +0,0 @@
-package com.learnzilla.backend.postmarks;
-
-public class Student {
-    public String id;
-    public String name;
-    public String grade;
-}
