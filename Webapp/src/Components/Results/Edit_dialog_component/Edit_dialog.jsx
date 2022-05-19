8dd043f734734266f6a587fbf57f247094603a31
waruniLalendra <warunilalendra@gmail.com>
2022-05-19 08:07:36 +0530

    Result-edit dialog box component UI created

---
+++ Webapp/src/Components/Results/Edit_dialog_component/Edit_dialog.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Edit_dialog.css'
+
+
+function Update_dialog() {
+
+    return (
+        <div className="container-edit">
+            <div className="wrap-edit">
+                <div id="title-question">
+                    <p>Are you sure you want to edit this result?</p>
+                </div>
+                <div id="btn-group">
+                    <span id="btn-edit"><button type="button" class="btn btn-success">Edit</button></span>
+                    <span id="btn-cancel"><button type="button" class="btn btn-danger">Cancel</button></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Update_dialog;
