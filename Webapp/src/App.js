5ddaac4b37448157773bd0ecdcb66d8720853e77
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-05-20 00:51:39 +0530

    Added rest API connection for register and enroll

--- Webapp/src/App.js
+++ Webapp/src/App.js
@@ -8,6 +8,7 @@ import Update_dialog from "./Components/Results/Update_dialog_component/Update_d
 import Edit_dialog from "./Components/Results/Edit_dialog_component/Edit_dialog";
 import Delete_dialog from "./Components/Results/Delete_dialog_component/Delete_dialog";
 import Announcement from "./Components/Announcement/Announcemnet";
+import Enroll from "./Components/Enroll/Enroll";
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
               <Route exact path="/Login" element={<Login/>} />
               <Route path="/Register"  element={<Register/>}/>
               <Route path="/Course"  element={<Course/>}/>
+              <Route path="/Enroll"  element={<Enroll/>}/>
               <Route path="/Results"  element={<Results/>}/>
               <Route path="/Update_dialog"  element={<Update_dialog/>}/>
               <Route path="/Edit_dialog"  element={<Edit_dialog/>}/>
