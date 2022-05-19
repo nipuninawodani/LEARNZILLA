c562d5699e442320223cb98129fa325b4f0cd767
waruniLalendra <warunilalendra@gmail.com>
2022-05-19 07:44:43 +0530

    Result-update dialog box component UI created

--- Webapp/src/Components/Results/Update_dialog_component/Update_dialog.jsx
+++ Webapp/src/Components/Results/Update_dialog_component/Update_dialog.jsx
@@ -8,7 +8,13 @@ function Update_dialog() {
     return (
         <div className="container-update">
             <div className="wrap-update">
-                Update
+                <div id="title-question">
+                    <p>Are you sure you want to update this result?</p>
+                </div>
+                <div id="btn-group">
+                    <span id="btn-update"><button type="button" class="btn btn-success">Update</button></span>
+                    <span id="btn-cancel"><button type="button" class="btn btn-danger">Cancel</button></span>
+                </div>
             </div>
         </div>
     )
