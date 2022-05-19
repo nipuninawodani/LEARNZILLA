c249b69916886f1b03544afb7f7d90a6a482cc81
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-05-20 02:08:47 +0530

    Added file uploader

--- Backend/src/main/java/com/learnzilla/backend/lectureresources/LectureResourceController.java
+++ Backend/src/main/java/com/learnzilla/backend/lectureresources/LectureResourceController.java
@@ -1,10 +1,12 @@
 package com.learnzilla.backend.lectureresources;
 
+import com.learnzilla.backend.fileUploader.FileUploader;
 import com.learnzilla.backend.models.Lecture;
 import com.learnzilla.backend.models.LectureResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -51,5 +53,11 @@ public class LectureResourceController {
     public void deleteLectureResource(@RequestBody LectureResource lectureResourceData) {
         lectureResourceRepository.deleteAllByLectureresourseid(lectureResourceData.getLectureresourseid());
     }
+
+    @PostMapping("/lectureResource/file")
+    public void uploadLectureResourceFile(@RequestParam("file") MultipartFile file) {
+        new FileUploader(file , "1");
+    }
+
 
 }
