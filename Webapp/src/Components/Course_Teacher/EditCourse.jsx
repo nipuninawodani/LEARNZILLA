69b42da96a11f038f6ccbfd99fb956e63f4c0b74
maheladissa <62756370+maheladissa@users.noreply.github.com>
2022-05-20 20:00:26 +0530

    connected add and edit courses to backend

--- Webapp/src/Components/Course_Teacher/EditCourse.jsx
+++ Webapp/src/Components/Course_Teacher/EditCourse.jsx
@@ -2,9 +2,34 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { Link } from "react-router-dom";
 import './Course_Teacher.css'
+import {useState} from "react";
+import CourseService from "../../services/CourseService";
 
 
 function Course_edit() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [course_code, setCourse_code] = useState('');
+    const [academic_year, setAcademic_year] = useState('');
+    const [level, setLevel] = useState('');
+    const [semester, setSemester] = useState('');
+    const [language, setLanguage] = useState('');
+    const [teacher_id, setTeacher_id] = useState('1');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const course = {title,description,course_code,academic_year,level,semester,language,teacher_id}
+
+        CourseService.createCourse(course).then((response) => {
+
+            console.log(response.data)
+
+        }).catch(error =>{
+            console.log(error)
+        })
+    }
 
     return (
         <div className="container-course_teacher">
@@ -24,31 +49,31 @@ function Course_edit() {
 
                     <div className="mb-3">
                         <label htmlFor="courseTitle" className="form-label">Course Title</label>
-                        <input type="email" className="form-control" id="courseTitle"/>
+                        <input type="email" className="form-control" id="courseTitle" value={title} onChange={e => { setTitle(e.target.value) }}/>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="courseDescription" className="form-label">Course Description</label>
-                        <textarea className="form-control" id="courseDescription"/>
+                        <textarea className="form-control" id="courseDescription" value={description} onChange={e => { setDescription(e.target.value) }}/>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="courseLanguage" className="form-label">Course Language</label>
-                        <input type="text" className="form-control" id="courseLanguage"/>
+                        <input type="text" className="form-control" id="courseLanguage" value={language} onChange={e => { setLanguage(e.target.value) }}/>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="courseLevel" className="form-label">Course Level</label>
-                        <input type="text" className="form-control" id="courseLevel"/>
+                        <input type="text" className="form-control" id="courseLevel" value={level} onChange={e => { setLevel(e.target.value) }}/>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="courseSemester" className="form-label">Course Semester</label>
-                        <input type="text" className="form-control" id="courseSemester"/>
+                        <input type="text" className="form-control" id="courseSemester" value={semester} onChange={e => { setSemester(e.target.value) }}/>
                     </div>
 
                     <div className="mb-3">
-                        <input className="form-control btn btn-danger" type="button" id="courseButton" value="Submit"/>
+                        <input className="form-control btn btn-danger" type="button" id="courseButton" value="Submit" onClick={(e) => handleSubmit(e)}/>
                     </div>
 
                 </form>
