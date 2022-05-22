fe0d70fc4d81ffef72d50bdc6be72de5ff776d0a
Sachin-Tharaka <tharakasachin98@gmail.com>
2022-05-22 13:50:00 +0530

    Resolve mail sending according to the enrollmrnts

--- Backend/src/main/java/com/learnzilla/backend/announcements/AnnouncementRepository.java
+++ Backend/src/main/java/com/learnzilla/backend/announcements/AnnouncementRepository.java
@@ -1,6 +1,6 @@
 package com.learnzilla.backend.announcements;
 
-import com.learnzilla.backend.signup.models.Student;
+import com.learnzilla.backend.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,6 +9,9 @@ import java.util.Optional;
 
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+
+    @Query("SELECT a FROM Announcement a WHERE a.course_code = ?1 ORDER BY a.id")
+    List<Announcement> findAnnouncementByCourse_code(String course_code);
 
 }
 
--- Backend/src/main/java/com/learnzilla/backend/enrollments/EnrollmentController.java
+++ Backend/src/main/java/com/learnzilla/backend/enrollments/EnrollmentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class EnrollmentController {
 
@@ -26,19 +27,25 @@ public class EnrollmentController {
     }
 
 
-    @GetMapping("/enrollment/course_code={course_code}&academic_year={academic_year}")
+    @GetMapping("/enrollment/gets/course_code={course_code}&academic_year={academic_year}")
     public ResponseEntity<List<Enrollment>> getEnrollmentByCourse(@PathVariable String course_code , @PathVariable String academic_year){
         List<Enrollment> enrollment = enrollmentRepository.findBycourse_codeAndacademic_year(course_code , academic_year);
         return ResponseEntity.ok(enrollment);
     }
 
-    @GetMapping("/enrollment/student_id={student_id}")
+    @GetMapping("/enrollment/gets/course_code={course_code}")
+    public ResponseEntity<List<Enrollment>> getEnrollmentByCourse(@PathVariable String course_code){
+        List<Enrollment> enrollment = enrollmentRepository.findBycourse_code(course_code);
+        return ResponseEntity.ok(enrollment);
+    }
+
+    @GetMapping("/enrollment/get/student_id={student_id}")
     public ResponseEntity<List<Enrollment>> getEnrollmentById(@PathVariable Integer student_id){
         List<Enrollment> enrollment = enrollmentRepository.findByStudent_id(student_id);
         return ResponseEntity.ok(enrollment);
     }
 
-    @GetMapping("/enrollment/{enrollmentid}")
+    @GetMapping("/enrollment/get/{enrollmentid}")
     public ResponseEntity<Enrollment> getEnrollmentByStudent(@PathVariable Long enrollmentid){
         Enrollment enrollment = enrollmentRepository.findByEnrollmentid(enrollmentid);
         return ResponseEntity.ok(enrollment);
