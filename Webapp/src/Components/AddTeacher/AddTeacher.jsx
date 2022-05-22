f2a18ea04366198fcaa465cb21bc24d4dad98f5d
Sachin-Tharaka <tharakasachin98@gmail.com>
2022-05-22 15:44:05 +0530

    Add/edit teacher components

---
+++ Webapp/src/Components/AddTeacher/AddTeacher.jsx
@@ -0,0 +1,45 @@
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { Link } from "react-router-dom";
+import './Course_Teacher.css'
+
+
+function Teacher_add() {
+
+    return (
+        <div className="container-course_teacher">
+            <div className="wrap-course_teacher">
+                <h2 className="text-center">Add Teacher</h2>
+
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="teacherName" className="form-label">Teacher ID</label>
+                        <input type="number" className="form-control" id="teacherName"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="firstName" className="form-label">Firstname</label>
+                        <input type="text" className="form-control" id="firstName"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="lastName" className="form-label">Lastname</label>
+                        <input type="text" className="form-control" id="lastName"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <input className="form-control btn btn-danger" type="button" id="courseButton" value="Submit"/>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Teacher_add;
