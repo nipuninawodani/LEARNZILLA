d8cdf19f2aedc1111fd53f2c5414bc30e875c4d7
Isuruni Rathnayaka <62780506+Isuruni@users.noreply.github.com>
2022-05-22 02:09:45 -0700

    JWT authentication updation

--- Backend/src/main/java/com/learnzilla/backend/register_login/controllers/StudentController.java
+++ Backend/src/main/java/com/learnzilla/backend/register_login/controllers/StudentController.java
@@ -54,9 +54,10 @@ public class StudentController {
 
     @PostMapping("/student/login")
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
         Authentication authentication= authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        authenticationRequest.getEmail(),passwordEncoder.encode(authenticationRequest.getPassword()))
+                        authenticationRequest.getEmail(),authenticationRequest.getPassword())
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
