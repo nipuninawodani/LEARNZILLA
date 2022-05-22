a68e33440f0dda4ec478f3f246b244f55ddaacd7
Isuruni Rathnayaka <62780506+Isuruni@users.noreply.github.com>
2022-05-21 18:12:42 -0700

    JWT authentication updation

--- Backend/src/main/java/com/learnzilla/backend/LearnzillaApplication.java
+++ Backend/src/main/java/com/learnzilla/backend/LearnzillaApplication.java
@@ -3,6 +3,9 @@ package com.learnzilla.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LearnzillaApplication {
@@ -13,4 +16,9 @@ public class LearnzillaApplication {
 		SpringApplication.run(LearnzillaApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 }
