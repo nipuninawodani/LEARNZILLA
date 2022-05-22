0f64a67585224562549dbd015a51d604d6d36a23
waruniLalendra <warunilalendra@gmail.com>
2022-05-22 08:20:27 +0530

    Home page UI for students created

--- Webapp/src/App.js
+++ Webapp/src/App.js
@@ -13,6 +13,7 @@ import Announcement_creation from "./Components/Announcement_creation/Announceme
 import Enroll from "./Components/Enroll/Enroll";
 import AddCourse from "./Components/Course_Teacher/AddCourse";
 import EditCourse from "./Components/Course_Teacher/EditCourse";
+import Home from "./Components/Home/Home";
 
 
 
@@ -38,6 +39,7 @@ function App() {
               <Route path="/Announcement_creation" element={<Announcement_creation/>} />
               <Route exact path="/Add_Course" element={<AddCourse/>} />
               <Route exact path="/Edit_Course" element={<EditCourse/>} />
+              <Route exact path="/Home" element={<Home/>} />
 
             </Routes> 
   
